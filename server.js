88c3b5b0fcc1292061d3687cbe63dd2de1fb32e2
OrangeMan <lenvampir@yandex.ru>
2018-05-13 20:18:47 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const port = 3000;
+let app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Привет, Юзер!</h1>');
+  res.send({
+    name: 'Egor',
+    likes: [
+      'Mushrooms',
+      'Books'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('<a href="http://localhost:3000/help.html">To bigger about page<a>');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Something gone wrong'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
